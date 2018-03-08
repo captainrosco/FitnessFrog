542641d09c5c91a4663eee9293d0c5c0238db479
Daron Short <daron.short@hotmail.com>
2018-03-08 17:48:14 -0600

    Updated Activity Field to use DropdownList

--- Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -44,6 +44,9 @@ namespace Treehouse.FitnessFrog.Controllers
                 Date = DateTime.Today
             };
 
+            ViewBag.SelectListItems = new SelectList(
+                Data.Data.Activities, "Id", "Name");
+
             return View(entry);
         }
 
@@ -54,6 +57,9 @@ namespace Treehouse.FitnessFrog.Controllers
                 _entriesRepository.AddEntry(entry);
                 return RedirectToAction("Index");
             }
+
+            ViewBag.SelectListItems = new SelectList(
+                Data.Data.Activities, "Id", "Name");
 
             return View(entry);
         }
