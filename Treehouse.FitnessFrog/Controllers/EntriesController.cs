ff24c666dedd67c18f643f1aa95a45f1d51da74f
Daron Short <daron.short@hotmail.com>
2018-03-07 18:41:34 -0600

    Updated Form Post Method

--- Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -43,8 +43,9 @@ namespace Treehouse.FitnessFrog.Controllers
             return View();
         }
 
-        [ActionName("Add"), HttpPost]
-        public ActionResult AddPost() {
+        [HttpPost]
+        public ActionResult Add(DateTime? date, int? activityId, double? duration, Entry.IntensityLevel? intensity, bool? exlude, string notes) {
+
             return View();
         }
 
